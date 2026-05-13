b4f9b00da63f2829ef83387b7dc7caf624fb376b
Putrifzhrhma <putri.fauziah206@gmail.com>
2026-05-13 20:54:06 +0700

    update mvc

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 define('BASEURL', 'http://localhost/ALS1/ALS/public');
-require_once __DIR__ . '/../app/controllers/UserController.php';
+require_once __DIR__ . '/app/controllers/UserController.php';
 $controller = new UserController();
 $page = isset($_GET['page']) ? $_GET['page'] : 'home';
 switch ($page) {
