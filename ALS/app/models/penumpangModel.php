6256a9a3c77242ff15ac5e9716dd2a4b338cc8d5
Putrifzhrhma <putri.fauziah206@gmail.com>
2026-05-18 23:36:59 +0700

    update mvc

--- ALS/app/models/penumpangModel.php
+++ ALS/app/models/penumpangModel.php
@@ -17,4 +17,27 @@ class PenumpangModel {
 
         return $stmt;
     }
+
+    public function getAllPenumpang() {
+        $query = 'SELECT * FROM ' . $this->table . ' ORDER BY created_at DESC';
+        $stmt = $this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getPenumpangById($id) {
+        $query = 'SELECT * FROM ' . $this->table . ' WHERE id = :id LIMIT 1';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function suspendPenumpang($id) {
+        
+        $query = 'UPDATE ' . $this->table . ' SET status = "suspended" WHERE id = :id';
+        $stmt = $this->conn->prepare($query);
+        $stmt->bindParam(':id', $id);
+        return $stmt->execute();
+    }
 }
